ddfe16f4376b3530f7b2b1a5b30b3b21392db391
Maria Gabriela <doupanda.com@gmail.com>
2021-02-18 11:49:29 -0300

    Initial Commit

---
+++ Udder2021/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Udder2021
+//
+//  Created by Maria on 18/02/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
