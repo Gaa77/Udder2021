3adebab25fe85af24df28eb984e6d6f5db9dcd3e
Maria Gabriela <doupanda.com@gmail.com>
2021-02-20 14:34:09 -0300

    Add view will appear

---
+++ Udder2021/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Udder2021
+//
+//  Created by Maria on 20/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+   }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        
+    }
+}
